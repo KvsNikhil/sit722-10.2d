08c54f5e54eae874e5a65735523520c9c68ce506
KVSNIKHIL1998 <kvs.nikhil1998@gmail.com>
2025-09-22 16:52:55 +1000

    tests: export PRODUCT_SERVICE_URL and fix status update assignment

--- backend/order_service/app/main.py
+++ backend/order_service/app/main.py
@@ -42,6 +42,11 @@ logger.info(
     f"Order Service: Configured to communicate with Customer Service at: {CUSTOMER_SERVICE_URL}"
 )
 
+#Adding
+PRODUCT_SERVICE_URL = os.getenv("PRODUCT_SERVICE_URL", "http://localhost:8003")
+logger.info(
+    f"Order Service: Configured to communicate with Product Service at: {PRODUCT_SERVICE_URL}"
+)
 
 # --- RabbitMQ Configuration ---
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
@@ -545,7 +550,7 @@ async def update_order_status(
             status_code=status.HTTP_404_NOT_FOUND, detail="Order not found"
         )
 
-    db_order.status = new_status
+    db_order.status = new_status.status
 
     try:
         db.add(db_order)
